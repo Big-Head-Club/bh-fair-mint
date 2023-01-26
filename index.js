0f90f4681466f996acde3ab1ca8ba4b4f3dee1e5
Josh Robar <josh@bighead.club>
2023-01-26 14:49:33 -0400

    Adding getRandomIndicesFromBlockHash function

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 import { Keccak } from 'sha3'
+import { ethers } from 'ethers'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const INPUT_PATH = path.join(__dirname, 'input')
+
+/**
+ *
+ * PROVENANCE HASH
+ *
+ */
 
 /**
  * Returns a "provenance hash" of the files in a given `directory`. This hash
@@ -30,3 +37,42 @@ function getProvenanceHash(directory) {
 }
 
 console.log('Provenance hash:', getProvenanceHash(INPUT_PATH))
+
+/**
+ *
+ * THE SHUFFLE
+ *
+ */
+
+const initialArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const BLOCK_HASH =
+  '0x45711a07caee26dbaf62e2b2cc2f7834eedcc19026be6a07dd48c6e36271d2fc'
+
+/**
+ * Returns an array of random indices within the provided `array`, using the
+ * supplied `blockHash` to seed the randomization.
+ */
+function getRandomIndicesFromBlockHash(array, blockHash) {
+  let randomIndices = []
+  const blockHashBigNumber = ethers.BigNumber.from(blockHash)
+
+  array.forEach(i => {
+    // 1 - Add `array` item `i` to `blockHash`
+    const iBigNumber = ethers.BigNumber.from(i)
+    const iPlusBlockHashBigNumber = iBigNumber.add(blockHashBigNumber)
+
+    // 2 - Take the keccak256 hash of the above result
+    const hash = new Keccak(256)
+      .update(iPlusBlockHashBigNumber._hex.substring(2)) // Omit the '0x' prefix
+      .digest('hex')
+
+    const hashBigNumber = ethers.BigNumber.from(`0x${hash}`)
+
+    // 3 - Return the result modulo the length of the `array`
+    randomIndices.push(hashBigNumber.mod(array.length).toNumber())
+  })
+
+  return randomIndices
+}
+
+console.log(getRandomIndicesFromBlockHash(initialArray, BLOCK_HASH))
